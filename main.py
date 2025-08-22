7c2e80450bfc901a59e21926edf07ec28e938e06
Gregor Sondermeier <GregorSondermeier@users.noreply.github.com>
2025-08-22 21:42:45 +0200

    code formatting

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, flash, redirect, request, render_template
 from models.contact import Contact
 
 Contact.load_db()
 
 # ========================================================
 # Flask App
-# ========================================================
+# ========================================================+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect('/contacts', 301)
+  return redirect('/contacts', 301)
 
 @app.route('/contacts')
 def contacts():
-    search = request.args.get('q')
-    if search is not None:
-        contacts_set = Contact.search(search)
-    else:
-        contacts_set = Contact.all()
-    return render_template("contacts.html", contacts=contacts_set)
+  search = request.args.get('q')
+  if search is not None:
+    contacts_set = Contact.search(search)
+  else:
+    contacts_set = Contact.all()
+  return render_template("contacts_list.html", contacts=contacts_set)
 
 if __name__ == "__main__":
-    # For development convenience; in production use a WSGI server.
-    app.run(debug=True)
+  # For development convenience; in production use a WSGI server.
+  app.run(debug=True)
